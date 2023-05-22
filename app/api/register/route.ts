3d71280c5743358ed2b272351eea22487d74f3d7
Suhant <72059795+Wolly-code@users.noreply.github.com>
2023-05-22 14:35:12 +0545

    Modified Databse Url Path
    Made changes to the URL of the database

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,18 +4,19 @@ import bcrypt from "bcrypt";
 import prisma from "@/app/libs/prismadb";
 
 export async function POST(
-  request: Request, 
+  request: Request,
 ) {
   const body = await request.json();
-  const { 
+
+  const {
     email,
     name,
     password,
-   } = body;
+  } = body;
 
-   const hashedPassword = await bcrypt.hash(password, 12);
+  const hashedPassword = await bcrypt.hash(password, 12);
 
-   const user = await prisma.user.create({
+  const user = await prisma.user.create({
     data: {
       email,
       name,
